f90a2e054dee06110b2fa5ef8fcbd5ef04425e45
azizhadiid <azizalhadiid88@gmail.com>
2025-06-03 21:08:12 +0700

    Ada Folder Baru dan Baru mulai UI Login dan Register

---
+++ Bukuku/Bukuku/Login.Designer.cs
@@ -0,0 +1,92 @@
+﻿namespace Bukuku
+{
+    partial class Login
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            panel1 = new Panel();
+            label2 = new Label();
+            pictureBox1 = new PictureBox();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            SuspendLayout();
+            // 
+            // panel1
+            // 
+            panel1.BackColor = Color.Teal;
+            panel1.Dock = DockStyle.Left;
+            panel1.Location = new Point(0, 0);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(466, 725);
+            panel1.TabIndex = 0;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Montserrat", 16.125F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label2.ForeColor = Color.Teal;
+            label2.Location = new Point(819, 9);
+            label2.Name = "label2";
+            label2.Size = new Size(204, 60);
+            label2.TabIndex = 1;
+            label2.Text = "Bukuku";
+            label2.Click += this.label2_Click;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Location = new Point(757, 223);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(200, 100);
+            pictureBox1.TabIndex = 2;
+            pictureBox1.TabStop = false;
+            // 
+            // Login
+            // 
+            AutoScaleDimensions = new SizeF(16F, 33F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = SystemColors.ButtonHighlight;
+            ClientSize = new Size(1350, 725);
+            Controls.Add(pictureBox1);
+            Controls.Add(label2);
+            Controls.Add(panel1);
+            Font = new Font("Montserrat SemiBold", 8.999999F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            FormBorderStyle = FormBorderStyle.None;
+            Margin = new Padding(4, 3, 4, 3);
+            Name = "Login";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Panel panel1;
+        private Label label2;
+        private PictureBox pictureBox1;
+    }
+}
---
+++ Bukuku/Bukuku/Login.cs
@@ -0,0 +1,10 @@
+namespace Bukuku
+{
+    public partial class Login : Form
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+    }
+}
