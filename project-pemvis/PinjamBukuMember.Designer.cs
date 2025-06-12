fab5f7035557a71ba435de41ed7e9b560efdd41f
zuldhiya <ddzulfaqor09@gmail.com>
2025-06-12 16:32:03 +0700

    rapikan

--- project-pemvis/PinjamBukuMember.Designer.cs
+++ project-pemvis/PinjamBukuMember.Designer.cs
@@ -250,7 +250,7 @@
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(1054, 792);
+            ClientSize = new Size(1054, 723);
             Controls.Add(FormPeminjaman);
             Controls.Add(lblSelamatDatang);
             Controls.Add(DaftarBuku);
