fa0c177ea91bcf15193822474e2551744655d030
azizhadiid <azizalhadiid88@gmail.com>
2025-06-13 15:04:20 +0700

    System Update Profile is Begin

--- project-pemvis/PinjamBukuMember.cs
+++ project-pemvis/PinjamBukuMember.cs
@@ -7,14 +7,29 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using MySql.Data.MySqlClient;
 
 namespace project_pemvis
 {
-    public partial class PinjamBukuMember: Form
+    public partial class PinjamBukuMember : Form
     {
-        public PinjamBukuMember()
+        private int userId;
+        public PinjamBukuMember(int userIdFromLogin)
         {
             InitializeComponent();
+            this.userId = userIdFromLogin;
+        }
+
+        private void btnProfil_Click(object sender, EventArgs e)
+        {
+            Profil profilForm = new Profil(userId); // Pastikan "Profil" adalah nama form yang benar
+            profilForm.Show();
+            this.Hide(); // Menyembunyikan form saat ini
+        }
+
+        private void PinjamBukuMember_Load(object sender, EventArgs e)
+        {
+
         }
     }
 }
